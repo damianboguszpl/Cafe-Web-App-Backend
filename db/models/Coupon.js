1e11ad9f2cd09111ccfc6a1a60011d4c8e19bf81
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-07-06 14:39:31 +0200

    created models; relations not finished

---
+++ db/models/Coupon.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const Coupon = sequelize.define("Coupon", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        value: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        point_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        is_available: {
+            type: DataTypes.TINYINT,
+            allowNull: false,
+        }
+    }, {
+        tableName: 'coupons'
+    })
+
+    Coupon.associate = (models) => {
+        Coupon.belongsTo(models.Product)
+        Coupon.belongsToMany(models.User, {
+            through: "UserCoupon"})
+    }
+
+    return Coupon
+}
