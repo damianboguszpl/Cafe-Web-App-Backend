b2969c77db5e059d10e48608d0f689d8d57562eb
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-10-18 21:34:59 +0200

    finished role verification

--- middlewares/verifyJWT.js
+++ middlewares/verifyJWT.js
@@ -1,37 +1,18 @@
 const jwt = require("jsonwebtoken")
 
-// const verifyToken = (req, res, next) => {
-//     const accessToken = req.header("accessToken")
-
-//     if (!accessToken) {
-//         return res.json({ error: "Nie jesteś zalogowany!" })
-//     }
-
-//     try {
-//         const validToken = verify(accessToken, "34qwereawdq4we3w3eqf7y6uhesecerttoken");
-//         req.user = validToken;
-//         if (validToken) {
-//             return next();
-//         }
-//     } catch (err) {
-//         return res.json({ error: err });
-//     }
-
-// }
-
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if (!authHeader?.startsWith('Bearer ')) 
         return res.sendStatus(401);
 
     const token = authHeader.split(' ')[1];
-    // console.log(token)
+    console.log(token)
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
-            if (err) return
-                res.sendStatus(403); //invalid token
+            if (err) 
+                return res.sendStatus(403); //invalid token
             req.user = decoded.user.email;
             req.RoleId = decoded.user.RoleId;
             next();
