cc233061ec898482c567dc221bd4f18f96d7cabf
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-10-18 21:34:34 +0200

    finished role verification

--- middlewares/verifyRole.js
+++ middlewares/verifyRole.js
@@ -1,12 +1,16 @@
 const verifyRole = (...allowedRoles) => {
     return (req, res, next) => {
-        console.log("1")
-        if (!req?.role)
+        // console.log("1")
+        // console.log(allowedRoles)
+        // console.log(req.RoleId)
+        if (!req?.RoleId)
             return res.sendStatus(401);
+        // console.log("id:" + req.RoleId)
 
         const rolesArray = [...allowedRoles];
-        const result = req.role.map(role => rolesArray.includes(role)).find(val => val === true);
-        console.log("2")
+        //const result = req.role.map(role => rolesArray.includes(role)).find(val => val === true);
+        const result = rolesArray.includes(req.RoleId)
+        // console.log("2")
         if (!result)
             return res.sendStatus(401);
         next();
