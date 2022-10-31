efe27f5975d551f9d6f12fa16a89da1e3f9cfd71
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-10-31 10:15:17 +0100

    rollback changes

--- routes/Users.js
+++ routes/Users.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const controller = require('../controllers/UserController')
 const { verifyJWT } = require("../middlewares/verifyJWT")
 const verifyRole = require("../middlewares/verifyRole")
+const ROLE_LIST = require('../config/role_list')
 
 // get all Users
-router.get('/', controller.getAll)
+router.get('/', verifyJWT, verifyRole(ROLE_LIST.admin, ROLE_LIST.employee), controller.getAll)
 
 // get User by id
 router.get('/:id', controller.getById)
@@ -21,15 +22,15 @@ router.post('/login', controller.login)
 
 // // validate login
 // router.get('/auth', validateToken, controller.validateToken)
-router.post('/auth', controller.auth)
+// router.post('/auth', controller.auth)
 
 // get Users by email
-router.get('/email/:email', controller.getByEmail)
+router.get('/email/:email', verifyJWT, verifyRole(ROLE_LIST.admin, ROLE_LIST.employee), controller.getByEmail)
 
 // get Users by phone
-router.get('/phone/:phone', controller.getByPhone)
+router.get('/phone/:phone', verifyJWT, verifyRole(ROLE_LIST.admin, ROLE_LIST.employee), controller.getByPhone)
 
 // get Users by RoleId
-router.get('/role/:roleid', controller.getByRoleId)
+router.get('/role/:roleid', verifyJWT, verifyRole(ROLE_LIST.admin), controller.getByRoleId)
 
 module.exports = router
