5ce798ae9f8d320a38c506e622a3a34cc017b4e2
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-11-16 21:03:29 +0100

    add password reset enpoints & nodemailer

--- routes/Mailer.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/MailerController')
-const { verifyJWT } = require("../middlewares/verifyJWT")
-
-// Send an email
-router.post("/", verifyJWT, controller.send)
-
-module.exports = router
