b4ad5f3b6570ee78c1ce89c1ae8d79d31ffd1fc7
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-11-23 15:14:54 +0100

    add UserCouponStatus model

---
+++ db/models/UserCouponStatus.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+    const UserCouponStatus = sequelize.define("UserCouponStatus", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    }, {
+        tableName: 'user_coupon_statuses'
+    })
+
+    UserCouponStatus.associate = (models) => {
+        UserCouponStatus.hasMany(models.UserCoupon, {onDelete: 'cascade'})
+    }
+
+    return UserCouponStatus
+}
