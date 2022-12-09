0108dd71c2c3736a77f645b8b9e7d01d14ba647c
damianboguszpl <damian.bogusz@pollub.edu.pl>
2022-12-09 18:35:45 +0100

    associate OrderDetails with UserCoupon

--- db/models/OrderDetails.js
+++ db/models/OrderDetails.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     OrderDetails.associate = (models) => {
         OrderDetails.belongsTo(models.OrderHeader)
         OrderDetails.belongsTo(models.Product)
+        OrderDetails.belongsTo(models.UserCoupon)
     }
 
     return OrderDetails
