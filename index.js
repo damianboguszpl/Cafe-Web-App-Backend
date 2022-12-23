e95f8637f968dbf4a998781b5b5f1672297c12fb
Paweł Ciszewski <kardahim@gmail.com>
2022-12-23 18:48:51 +0100

    add documentation for users route

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cookieParser = require("cookie-parser")
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:3001/api-info"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
