811d4a5ee6f6b40b8e4670ac06f8f83849f81c5a
Paweł Ciszewski <kardahim@gmail.com>
2022-12-24 14:22:06 +0100

    fix

--- config/swagger_options.js
+++ config/swagger_options.js
@@ -6,14 +6,14 @@ const options = {
             version: '1.0.0',
             description: 'API for web and mobile apps'
         },
+        schemes: ["http"],
+        servers: [
+            {
+                url: 'http://localhost:3001/',
+                description: 'Development server',
+            },
+        ],
     },
-    schemes: ['http', 'https'],
-    servers: [
-        {
-            url: 'http://localhost:3001',
-            description: 'Development server',
-        },
-    ],
     apis: ['./routes/*.js'],
 }
 
