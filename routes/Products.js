e295a46cec0e7c16be8d28a5e7b348ebd2b66fd0
Paweł Ciszewski <kardahim@gmail.com>
2022-12-24 18:26:04 +0100

    document products

--- routes/Products.js
+++ routes/Products.js
@@ -5,35 +5,216 @@ const { verifyJWT } = require("../middlewares/verifyJWT")
 const verifyRole = require("../middlewares/verifyRole")
 const ROLE_LIST = require('../config/role_list')
 
-// TODO: document
-// Create new Product
+/**
+ * @openapi
+ * /products:
+ *  post:
+ *      description: Endpoint for create new product
+ *      summary: Create product
+ *      tags:
+ *      - products
+ *      parameters:
+ *          - name: name
+ *            in: body
+ *            required: true
+ *          - name: size
+ *            in: body
+ *            required: true
+ *          - name: price
+ *            in: body
+ *            required: true
+ *          - name: allergen
+ *            in: body
+ *          - name: CategoryId
+ *            in: body
+ *            required: true
+ *          - name: ProductStatusId
+ *            in: body
+ *            required: true
+ *      responses:
+ *          201:
+ *              description: Created
+ *          400:
+ *              description: Bad request
+ */
 router.post("/", verifyJWT, verifyRole(ROLE_LIST.admin), controller.create)
 
-// Update Product
+/**
+ * @openapi
+ * /products/update/{id}:
+ *  put:
+ *      description: Endpoint for update existing product
+ *      summary: Update product
+ *      tags:
+ *      - products
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *          - name: name
+ *            in: body
+ *          - name: size
+ *            in: body
+ *          - name: price
+ *            in: body
+ *          - name: allergen
+ *            in: body
+ *          - name: CategoryId
+ *            in: body
+ *          - name: ProductStatusId
+ *            in: body
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.put("/update/:id", verifyJWT, verifyRole(ROLE_LIST.admin), controller.update)
 
-// Delete Product 
+/**
+ * @openapi
+ * /products/{id}:
+ *  delete:
+ *      description: Endpoint for delete existing product
+ *      summary: Delete product
+ *      tags:
+ *      - products
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ */
 router.delete(`/:id`, verifyJWT, verifyRole(ROLE_LIST.admin), controller.delete)
 
-// Get all Products
+/**
+ * @openapi
+ * /products:
+ *  get:
+ *      description: Endpoint for get all products
+ *      summary: Get all products
+ *      tags:
+ *      - products
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          404:
+ *              description: Not found
+ */
 router.get("/", controller.getAll)
 
-// Get all Products with associated Special Offers
+/**
+ * @openapi
+ * /products/specialoffers:
+ *  get:
+ *      description: Endpoint for get all Products with associated Special Offers
+ *      summary: Get all products with associated Special Offers
+ *      tags:
+ *      - products
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          404:
+ *              description: Not found
+ */
 router.get("/specialoffers", controller.getAllWithSpecialOffers)
 
-// Get all Products with associated Special Offers that have no coupons
+/**
+ * @openapi
+ * /products/withoutcoupons:
+ *  get:
+ *      description: Endpoint for get all Products without coupons
+ *      summary: Get all products without coupons
+ *      tags:
+ *      - products
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          404:
+ *              description: Not found
+ */
 router.get("/withoutcoupons", controller.getAllWithoutCoupons)
 
-// Get Product by id
+/**
+ * @openapi
+ * /products/{id}:
+ *  get:
+ *      description: Endpoint for get existing product by id
+ *      summary: Get product by id
+ *      tags:
+ *      - products
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.get("/:id", controller.getById)
 
-// Get Product by name
+/**
+ * @openapi
+ * /products/name/{name}:
+ *  get:
+ *      description: Endpoint for get existing product by name
+ *      summary: Get product by name
+ *      tags:
+ *      - products
+ *      parameters:
+ *          - name: name
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.get("/name/:name", controller.getByName)
 
-// Get Products by ProductStatusId
+/**
+ * @openapi
+ * /products/status/{id}:
+ *  get:
+ *      description: Endpoint for get existing product by status id
+ *      summary: Get product by status id
+ *      tags:
+ *      - products
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.get("/status/:id", controller.getByProductStatusId)
 
-// Get Products by CategoryId
+/**
+ * @openapi
+ * /products/category/{id}:
+ *  get:
+ *      description: Endpoint for get existing product by category id
+ *      summary: Get product by category id
+ *      tags:
+ *      - products
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.get("/category/:id", controller.getByCategoryId)
 
 module.exports = router
