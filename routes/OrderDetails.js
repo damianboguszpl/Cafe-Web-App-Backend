38794232c0278e6407f6f2fcc5b0c555ac7276af
Paweł Ciszewski <kardahim@gmail.com>
2022-12-24 15:21:34 +0100

    document order details

--- routes/OrderDetails.js
+++ routes/OrderDetails.js
@@ -5,25 +5,161 @@ const { verifyJWT } = require("../middlewares/verifyJWT")
 const verifyRole = require("../middlewares/verifyRole")
 const ROLE_LIST = require('../config/role_list')
 
-// Create new OrderDetails
+/**
+ * @openapi
+ * /orderdetails:
+ *  post:
+ *      description: Endpoint for create new order details
+ *      summary: Create order details
+ *      tags:
+ *      - orderdetails
+ *      parameters:
+ *          - name: transaction_price
+ *            in: body
+ *            required: true
+ *          - name: quantity
+ *            in: body
+ *            required: true
+ *          - name: OrderHeaderId
+ *            in: body
+ *            required: true
+ *          - name: ProductId
+ *            in: body
+ *            required: true
+ *      responses:
+ *          201:
+ *              description: Created
+ *          400:
+ *              description: Bad request
+ *          404:
+ *              description: Not found
+ */
 router.post("/", verifyJWT, verifyRole(ROLE_LIST.admin, ROLE_LIST.employee), controller.create)
 
-// Update OrderDetails
+/**
+ * @openapi
+ * /orderdetails/update/{id}:
+ *  put:
+ *      description: Endpoint for update order details
+ *      summary: Update order details
+ *      tags:
+ *      - orderdetails
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *          - name: transaction_price
+ *            in: body
+ *          - name: quantity
+ *            in: body
+ *          - name: OrderHeaderId
+ *            in: body
+ *          - name: ProductId
+ *            in: body
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ *          404:
+ *              description: Not found
+ */
 router.put("/update/:id", verifyJWT, verifyRole(ROLE_LIST.admin, ROLE_LIST.employee), controller.update)
 
-// Delete OrderDetails 
+/**
+ * @openapi
+ * /orderdetails/{id}:
+ *  delete:
+ *      description: Endpoint for delete order details
+ *      summary: Delete order details
+ *      tags:
+ *      - orderdetails
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          201:
+ *              description: Created
+ *          404:
+ *              description: Not found
+ */
 router.delete(`/:id`, verifyJWT, verifyRole(ROLE_LIST.admin, ROLE_LIST.employee), controller.delete)
 
-// Get all OrderDetails
+/**
+ * @openapi
+ * /orderdetails:
+ *  get:
+ *      description: Endpoint for get all order details
+ *      summary: Get all order details
+ *      tags:
+ *      - orderdetails
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          404:
+ *              description: Not found
+ */
 router.get("/", verifyJWT, controller.getAll)
 
-// Get OrderDetails by id
+/**
+ * @openapi
+ * /orderdetails/{id}:
+ *  get:
+ *      description: Endpoint for get order details by id
+ *      summary: Get order details by id
+ *      tags:
+ *      - orderdetails
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          404:
+ *              description: Not found
+ */
 router.get("/:id", verifyJWT, controller.getById)
 
-// Get OrderDetails by ProductId
+/**
+ * @openapi
+ * /orderdetails/product/{id}:
+ *  get:
+ *      description: Endpoint for get order details by product id
+ *      summary: Get order details by product id
+ *      tags:
+ *      - orderdetails
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          404:
+ *              description: Not found
+ */
 router.get("/product/:id", verifyJWT, controller.getByProductId)
 
-// Get OrderDetails by OrderHeaderId
+/**
+ * @openapi
+ * /orderdetails/orderheader/{id}:
+ *  get:
+ *      description: Endpoint for get order details by order header id
+ *      summary: Get order details by order header id
+ *      tags:
+ *      - orderdetails
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          404:
+ *              description: Not found
+ */
 router.get("/orderheader/:id", verifyJWT, controller.getByOrderHeaderId)
 
 module.exports = router
