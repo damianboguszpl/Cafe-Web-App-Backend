7b0a8b9f1e740ae77ad7c585572cbd2b3d30ed4e
Paweł Ciszewski <kardahim@gmail.com>
2022-12-24 14:50:15 +0100

    document categories

--- routes/Categories.js
+++ routes/Categories.js
@@ -5,25 +5,139 @@ const { verifyJWT } = require("../middlewares/verifyJWT")
 const verifyRole = require("../middlewares/verifyRole")
 const ROLE_LIST = require('../config/role_list')
 
-// Create new Category
+/**
+ * @openapi
+ * /categories:
+ *  post:
+ *      description: Endpoint for create new category
+ *      summary: Create category
+ *      tags:
+ *      - categories
+ *      parameters:
+ *          - name: name
+ *            in: body
+ *            required: true
+ *      responses:
+ *          201:
+ *              description: Created
+ *          400:
+ *              description: Bad request
+ */
 router.post("/", verifyJWT, verifyRole(ROLE_LIST.admin), controller.create)
 
-// Update Category
+/**
+ * @openapi
+ * /categories/update/{id}:
+ *  put:
+ *      description: Endpoint for update existing category
+ *      summary: Update category
+ *      tags:
+ *      - categories
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *          - name: name
+ *            in: body
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.put("/update/:id", verifyJWT, verifyRole(ROLE_LIST.admin), controller.update)
 
-// Delete Category 
+/**
+ * @openapi
+ * /categories/{id}:
+ *  delete:
+ *      description: Endpoint for delete category
+ *      summary: Delete category
+ *      tags:
+ *      - categories
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.delete(`/:id`, verifyJWT, verifyRole(ROLE_LIST.admin), controller.delete)
 
-// Get all Categories
+/**
+ * @openapi
+ * /categories:
+ *  get:
+ *      description: Endpoint for get all categories
+ *      summary: Get all categories
+ *      tags:
+ *      - categories
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.get("/", controller.getAll)
 
-// Get all Categories
+/**
+ * @openapi
+ * /categories/notempty:
+ *  get:
+ *      description: Endpoint for get all not empty categories
+ *      summary: Get not empty categories
+ *      tags:
+ *      - categories
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.get("/notempty", controller.getAllNotEmpty)
 
-// Get Category by id
+/**
+ * @openapi
+ * /categories/{id}:
+ *  get:
+ *      description: Endpoint for get category by id
+ *      summary: Get category by id
+ *      tags:
+ *      - categories
+ *      parameters:
+ *          - name: id
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.get("/:id", controller.getById)
 
-// Get Category by name
+/**
+ * @openapi
+ * /categories/name/{name}:
+ *  get:
+ *      description: Endpoint for get category by name
+ *      summary: Get category by name
+ *      tags:
+ *      - categories
+ *      parameters:
+ *          - name: name
+ *            in: path
+ *            required: true
+ *      responses:
+ *          200:
+ *              description: Ok
+ *          400:
+ *              description: Bad request
+ */
 router.get("/name/:name", controller.getByName)
 
 module.exports = router
