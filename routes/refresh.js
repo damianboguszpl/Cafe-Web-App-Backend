d65592818410c457bf50faf09392a782e4b537ca
Paweł Ciszewski <kardahim@gmail.com>
2022-12-26 13:10:21 +0100

    remove todo note

--- routes/refresh.js
+++ routes/refresh.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const RefreshTokenController = require('../controllers/RefreshTokenController')
 const { verifyJWT } = require("../middlewares/verifyJWT")
 
-// TODO: document 
 // I dont know the route of this
 router.get('/', RefreshTokenController.handleRefreshToken)
 module.exports = router
